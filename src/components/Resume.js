3c5ce6aa94d4104b8c7607451dd6a27ab0dee556
daigled <daigled1@gmail.com>
2020-04-25 21:43:02 -0500

    resume styles, page transitions

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,15 +1,61 @@
 import React from 'react'
+import './Resume.scss'
 
 export default function Resume() {
     return (
         <div id="resume">
             <h2 className="page-title">Resume</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> 
+            <div id="resume-container">
+                <section>
+                    <h2 className="section-header">Experience</h2>
+                    <div className="subsection">
+                        <h3 className="subsection-header">BBVA USA</h3>
+                        <h4 className="subsection-subheader">Software Engineer</h4>
+                        <ul>
+                            <li>Built and maintained a custom framework for test automation</li>
+                            <li>Developed full stack, CI/CD integrated web applications for internal use. Application stack: Angular 8.x UI, Spring Boot API, MongoDB database</li>
+                            <li>Provided training and assistance for testers who were new to automation</li>
+                            <li>Converted large applications to automation testing for regression tests and CI/CD pipelines for deployments</li>
+                        </ul>
+                    </div>
+                    <div className="subsection">
+                        <h3 className="subsection-header">BBVA USA</h3>
+                        <h4 className="subsection-subheader">Software Engineer</h4>
+                        <ul>
+                            <li>Built and maintained a custom framework for test automation</li>
+                            <li>Developed full stack, CI/CD integrated web applications for internal use. Application stack: Angular 8.x UI, Spring Boot API, MongoDB database</li>
+                            <li>Provided training and assistance for testers who were new to automation</li>
+                            <li>Converted large applications to automation testing for regression tests and CI/CD pipelines for deployments</li>
+                        </ul>
+                    </div>
+                </section>
+                <section>
+                    <h2 className="section-header">Experience</h2>
+                    <div className="subsection">
+                        <h3 className="subsection-header">BBVA USA</h3>
+                        <pre>Software Engineer</pre>
+                        <ul>
+                            <li>Built and maintained a custom framework for test automation</li>
+                            <li>Developed full stack, CI/CD integrated web applications for internal use. Application stack: Angular 8.x UI, Spring Boot API, MongoDB database</li>
+                            <li>Provided training and assistance for testers who were new to automation</li>
+                            <li>Converted large applications to automation testing for regression tests and CI/CD pipelines for deployments</li>
+                        </ul>
+                    </div>
+                </section>
+                <section>
+                    <h2 className="section-header">Experience</h2>
+                    <div className="subsection">
+                        <h3 className="subsection-header">BBVA USA</h3>
+                        <pre>Software Engineer</pre>
+                        <ul>
+                            <li>Built and maintained a custom framework for test automation</li>
+                            <li>Developed full stack, CI/CD integrated web applications for internal use. Application stack: Angular 8.x UI, Spring Boot API, MongoDB database</li>
+                            <li>Provided training and assistance for testers who were new to automation</li>
+                            <li>Converted large applications to automation testing for regression tests and CI/CD pipelines for deployments</li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
         </div>
     )
 }
