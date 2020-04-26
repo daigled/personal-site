571c62f55ed5c321a1d14e1bea27b9432662b130
daigled <daigled1@gmail.com>
2020-04-25 20:52:21 -0500

    mobile menu looks good

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+
+import Home from './components/Home'
+import About from './components/About'
+import Resume from './components/Resume'
+
+
+const routes = [
+  { path: '/', name: 'Home', Component: Home },
+  { path: '/about', name: 'About', Component: About },
+  { path: '/contact', name: 'Resume', Component: Resume },
+]
+
+// Hook
+const useWindowSize = () => {
+  const isClient = typeof window === 'object';
+
+  function getSize() {
+    return {
+      width: isClient ? window.innerWidth : undefined,
+      height: isClient ? window.innerHeight : undefined
+    };
+  }
+
+  const [windowSize, setWindowSize] = useState(getSize);
+
+  useEffect(() => {
+    if (!isClient) {
+      return false;
+    }
+    
+    function handleResize() {
+      setWindowSize(getSize());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+  return windowSize;
+}
 
 function App() {
+  const [ isLoaded, setIsLoaded ] = useState(false)
+  const size = useWindowSize()
+  const isMobile = size.width <= 797
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className={ isLoaded ? "App" : "App loading"}>
+        <Header isMobile={isMobile} routes={routes} />
+
+        <main>
+          <div className="container">
+            <div id="primary-layout">
+            {routes.map(({ path, Component }) => (
+              <Route key={path} exact path={path}>
+                {({ match }) => (
+                  <CSSTransition
+                    in={match != null}
+                    timeout={300}
+                    classNames="page"
+                    unmountOnExit
+                  >
+                    <div className="page">
+                      <Component />
+                    </div>
+                  </CSSTransition>
+                )}
+              </Route>
+            ))}
+            </div>
+          </div>
+        </main>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/layout/Header.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import { NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+
+import './Header.scss'
+
+
+
+export default function Header(props) {
+
+    const { routes, isMobile } = props
+    const [ menuVisible, setMenuVisible ] = useState(false);
+
+    const toggleMenuVisible = () => setMenuVisible(!menuVisible)
+
+    return (
+        <header id="site-header">
+            <div className="container">
+                <NavLink to="/"><img className="logo" src="https://placehold.it/50x50"/></NavLink>
+                <nav id="primary-navigation" className={ isMobile ? menuVisible ? "visible" : "" : "visible" }>
+                    <ul>
+                    {routes.map(route => (
+                        <li><NavLink
+                            key={route.path}
+                            to={route.path}
+                            onClick={e => isMobile ? toggleMenuVisible() : e.preventDefault()}
+                            activeClassName="active"
+                            exact                                           
+                        >
+                            {route.name}
+                        </NavLink></li>
+                        ))}
+                    </ul>
+                    <span className="menu-toggle" onClick={e => toggleMenuVisible()}><FontAwesomeIcon icon={faTimes} /></span>
+                </nav>
+                <span className="menu-toggle" onClick={e => toggleMenuVisible()}><FontAwesomeIcon icon={faBars} /></span>
+            </div>
+        </header>
+    )
+}
